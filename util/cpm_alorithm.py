c0b4ce76cf8d209ef552cf5bbf420dc7af8a6159
Michal Jurzak <juziu2002@gmail.com>
2024-03-14 14:33:31 +0100

    Added algorithm for cpm

---
+++ util/cpm_alorithm.py
@@ -0,0 +1,43 @@
+from critical import CriticalPath
+'''
+Algorithm that calculates the critical path given the inputs.
+Inputs: 
+- Action: List['str']
+- Duration: time['int']
+- Action sequence: action_numbers['int', 'int']
+'''
+# N - name
+# D - duration
+# B - begin node
+# E - end node
+
+# case 1
+Table = [
+    # N | D | B, E |
+    ('A', 3, (0, 1)), # 0
+    ('B', 4, (1, 2)), # 1
+    ('C', 6, (1, 3)), # 2
+    ('D', 7, (2, 4)), # 3
+    ('E', 1, (4, 6)), # 4
+    ('F', 2, (3, 6)), # 5
+    ('G', 3, (3, 5)), # 6
+    ('H', 4, (5, 6)), # 7
+    ('I', 1, (6, 7)), # 8
+    ('J', 2, (7, 8)), # 9
+]
+
+Action     = [item[0] for item in Table]
+Duration   = [item[1] for item in Table]
+Sequence   = [item[2] for item in Table]
+Sequence_b = [item[2][0] for item in Table]
+Sequence_e = [item[2][1] for item in Table]
+        
+# Driver code
+if __name__ == '__main__':
+    cpath = CriticalPath()
+    for i in range(len(Table)-1): cpath.add_node(i)
+    
+    cpath.add_edges(Sequence_b, Sequence_e, Duration)
+    cpath._print()
+    print(cpath.longest_path())
+    print(cpath.add_action_names(Action, Sequence))    
